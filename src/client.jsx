e38a488ba6c466c76e54880bd8307903b3e00469
ikpil <ikpil@naver.com>
2020-02-27 09:25:29 +0900

    폴더 구조 변경

--- src/client.jsx
+++ src/client.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root';
-import TicTacToe from './TicTacToe';
+import TicTacToe from './TicTacToe/TicTacToe';
 
 const HotLoader = hot(TicTacToe);
 
