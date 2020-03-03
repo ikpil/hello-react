13d39da219f8555c8f4073c36178319745d8d3a5
ikpil <ikpil@naver.com>
2020-03-03 09:38:50 +0900

    지회찾기 - 마우스 우 클륵 대응

--- src/MineSearch/MineSearch.jsx
+++ src/MineSearch/MineSearch.jsx
@@ -25,15 +25,22 @@ export const TableContext = createContext({
         [-1, -1, -1, -1, -1, -1, -1],
     ],
     dispatch: () => { },
+    halted: false,
 });
 
 const initialState = {
     tableData: [],
     timer: 0,
     result: 0,
+    halted: false,
 }
 
 export const START_GAME = 'START_GAME';
+export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NORMALIZE_CELL = 'NORMALIZE_CELL';
 
 const plantMine = (row, cell, mine) => {
     console.log(row, cell, mine);
@@ -68,12 +75,75 @@ const plantMine = (row, cell, mine) => {
 };
 
 const reducer = (state, action) => {
+    console.log(`reducer - ${action.type}`);
+
     switch (action.type) {
         case START_GAME:
             return {
                 ...state,
                 tableData: plantMine(action.row, action.cell, action.mine),
             }
+        case OPEN_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...tableData[action.row]];
+            tableData[action.row][action.cell] = CODE.OPENED;
+            return {
+                ...state,
+                tableData,
+            }
+        }
+        case CLICK_MINE: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+            return {
+                ...state,
+                tableData,
+                halted: true,
+            };
+        }
+        case FLAG_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (CODE.MINE === tableData[action.row][action.cell]) {
+                tableData[action.row][action.cell] = CODE.FLAG_MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.FLAG;
+            }
+
+            return {
+                ...state,
+                tableData,
+            };
+        }
+        case QUESTION_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (CODE.FLAG_MINE === tableData[action.row][action.cell]) {
+                tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.QUESTION;
+            }
+
+            return {
+                ...state,
+                tableData,
+            };
+        }
+        case NORMALIZE_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (CODE.QUESTION_MINE === tableData[action.row][action.cell]) {
+                tableData[action.row][action.cell] = CODE.MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.NORMAL;
+            }
+
+            return {
+                ...state,
+                tableData,
+            };
+        }
         default:
             return state;
     }
@@ -81,22 +151,22 @@ const reducer = (state, action) => {
 
 const MineSearch = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const { tableData, halted, timer, result } = state;
 
     const value = useMemo(() => {
         return {
-            tableData: state.tableData,
+            tableData: tableData,
+            halted: halted,
             dispatch,
         }
-    }, [state.tableData]);
-
-    console.log("asdfasdfasf");
+    }, [tableData, halted]);
 
     return (
         <TableContext.Provider value={value}>
             <Form />
-            <div>{state.timer}</div>
+            <div>{timer}</div>
             <Table />
-            <div>{state.result}</div>
+            <div>{result}</div>
         </TableContext.Provider>
     )
 };
--- src/MineSearch/Td.jsx
+++ src/MineSearch/Td.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { CODE, TableContext } from './MineSearch'
+import React, { useContext, useCallback } from 'react';
+import { CODE, TableContext, OPEN_CELL, CLICK_MINE, FLAG_CELL, QUESTION_CELL, NORMALIZE_CELL } from './MineSearch'
 
 const getTdStyle = (code) => {
     switch (code) {
@@ -12,7 +12,16 @@ const getTdStyle = (code) => {
             return {
                 background: 'white',
             }
-
+        case CODE.QUESTION_MINE:
+        case CODE.QUESTION:
+            return {
+                background: 'yellow',
+            }
+        case CODE.FLAG_MINE:
+        case CODE.FLAG:
+            return {
+                background: 'red',
+            }
         default:
             return {
                 background: 'white'
@@ -26,17 +35,75 @@ const getTdText = (code) => {
             return '';
         case CODE.MINE:
             return 'X';
+        case CODE.CLICKED_MINE:
+            return '펑';
+        case CODE.FLAG_MINE:
+        case CODE.FLAG:
+            return '!';
+        case CODE.QUESTION_MINE:
+        case CODE.QUESTION:
+            return '?';
         default:
             return '';
     }
 };
 
 const Td = ({ rowIdx, cellIdx }) => {
-    const { tableData } = useContext(TableContext);
+    const { tableData, dispatch, halted } = useContext(TableContext);
+
+    const onClickTd = useCallback((e) => {
+        console.log(`cell clicked - row(${rowIdx}) cell((${cellIdx})) data(${tableData[rowIdx][cellIdx]})`);
+
+        e.preventDefault();
+        if (halted) {
+            return;
+        }
+
+        switch (tableData[rowIdx][cellIdx]) {
+            case CODE.NORMAL:
+                dispatch({ type: OPEN_CELL, row: rowIdx, cell: cellIdx });
+                return
+            case CODE.MINE:
+                dispatch({ type: CLICK_MINE, row: rowIdx, cell: cellIdx });
+                return;
+            default:
+                return;
+        }
+    }, [tableData[rowIdx][cellIdx], halted]);
+
+    const onRightClickTd = useCallback((e) => {
+        console.log(`cell right clicked - row(${rowIdx}) cell((${cellIdx})) data(${tableData[rowIdx][cellIdx]})`);
+
+        e.preventDefault();
+        if (halted) {
+            return;
+        }
+
+        switch (tableData[rowIdx][cellIdx]) {
+            case CODE.NORMAL:
+            case CODE.MINE:
+                dispatch({ type: FLAG_CELL, row: rowIdx, cell: cellIdx });
+                return;
+
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+                dispatch({ type: QUESTION_CELL, row: rowIdx, cell: cellIdx });
+                return;
+
+            case CODE.QUESTION_MINE:
+            case CODE.QUESTION:
+                dispatch({ type: NORMALIZE_CELL, row: rowIdx, cell: cellIdx });
+                return;
+            default:
+                return;
+        }
+    }, [tableData[rowIdx][cellIdx], halted]);
 
     return (
         <td
             style={getTdStyle(tableData[rowIdx][cellIdx])}
+            onClick={onClickTd}
+            onContextMenu={onRightClickTd}
         >{getTdText(tableData[rowIdx][cellIdx])}
         </td>
     )
